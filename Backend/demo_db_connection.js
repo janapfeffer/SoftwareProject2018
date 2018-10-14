2196f7c6a8ebdc39eb550ff64db9df05e47c9929
Jana Lotta Pfeffer <jana.lotta.pfeffer01@sap.com>
2018-10-14 15:17:24 +0200

    Update Backend/demo_db_connection.js

--- Backend/demo_db_connection.js
+++ Backend/demo_db_connection.js
@@ -1,3 +1,10 @@
+/*
+1. The following code has to be executed on the mysql database in order for the code to work:
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
+2. mysql for node has to be installed before running the code:
+run "npm install mysql" in the command line
+3. see https://www.w3schools.com/nodejs/nodejs_mysql.asp for help
+*/
 var mysql = require("mysql");
 
 var con = mysql.createConnection({
@@ -12,7 +19,8 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
-/*
-The following code has to be executed on the mysql database in order for the code to work:
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
-*/
+var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
