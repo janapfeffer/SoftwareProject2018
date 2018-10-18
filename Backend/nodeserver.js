0fbf13566c7a33abbc7ecad176a79bcd1fd9f3ee
Jana Lotta Pfeffer <jana.lotta.pfeffer01@sap.com>
2018-10-18 11:59:20 +0200

    Update Backend/nodeserver.js

--- Backend/nodeserver.js
+++ Backend/nodeserver.js
@@ -1,7 +1,8 @@
 //https://www.w3schools.com/nodejs/nodejs_http.asp
-var http = require('http');
-var url = require('url');
+// var http = require('http');
+// var url = require('url');
 var mysql = require("mysql");
+var express = require("express");
 
 // connect to the database
 var con = mysql.createConnection({
@@ -18,7 +19,27 @@ con.connect(function(err) {
 
 //#######################################################################
 
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+// http.createServer(function (req, res) {
+//   res.write('Hello World!'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(8080); //the server object listens on port 8080
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/event_types', function (req, res) {
+  con.query("SELECT * FROM event_finder.event_types;", function (err, result, fields) {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
