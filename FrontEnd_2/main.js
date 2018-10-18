b8c87f8bceb5e3b33257c0d97c88b1eebf6c8edf
ToastMX <ToastMX@paulk.de>
2018-10-19 00:58:55 +0200

    aaaaallleeeeesssssss

--- FrontEnd_2/main.js
+++ FrontEnd_2/main.js
@@ -1,10 +1,8 @@
-new Vue({
-  el: '#app',
-  data() {
-    return {
+main = new Vue({
+  el: 'header',
+  data: {
       activeIndex: '1',
       activeIndex2: '1'
-    };
   },
   methods: {
     handleSelect(key, keyPath) {
@@ -12,3 +10,25 @@ new Vue({
     }
   }
   });
+
+eventlist = new Vue({
+  el: '#eventlist',
+  data: {
+    shownEvents: [
+      {
+        title: 'Semestereroeffnungsparty',
+        desc: 'Es ist wieder zeit',
+        time: 'Heute 17 Uhr',
+        place: 'Jungbusch'
+      },
+      {
+        title: 'Ausstellungseroeffnung',
+        desc: 'die Agyptische sammlung des Kulturinstituts laed ein',
+        time: 'Heute 19 Uhr',
+        place: 'Q2'
+      }
+    ]
+  },
+  methods: {
+  }
+});
