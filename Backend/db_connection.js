97c2c64807ec10cd45d894020948b386f66f2aaa
Jana Lotta Pfeffer <jana.lotta.pfeffer01@sap.com>
2018-10-18 17:49:41 +0200

    db_connection.js 18.10 BE Treffen
    Methoden teilweise implementiert

--- Backend/db_connection.js
+++ Backend/db_connection.js
@@ -17,10 +17,10 @@ con.query(sql, function (err, result) {
 */
 
 //https://www.w3schools.com/nodejs/nodejs_http.asp
-var http = require('http');
-var url = require('url');
 var mysql = require("mysql");
 var express = require("express");
+
+const db_name = "event_finder";
 
 // connection to the database
 var con = mysql.createConnection({
@@ -30,66 +30,124 @@ var con = mysql.createConnection({
   insecureAuth : true
 });
 
-// All things that should be executed upon executing this file should be done here
-function onInit(){
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to database.");
-  });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to database.");
+});
 
-  //https://www.w3schools.com/nodejs/nodejs_url.asp
-  http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    res.write(q.pathname); //write a response to the client
-    res.end(); //end the response
-  }).listen(8080); //the server object listens on port 8080
-}
 
 //#################Functions####################################################
 //returns all event_types entries
 function get_event_types() {
-  con.query("SELECT * FROM event_finder.event_types;", function (err, result) {
+  con.query(`SELECT * FROM ${db_name}.event_types;`, function (err, result) {
     if (err) {
       throw err;
     } else {
-      //console.log(result);
       return result;
     }
   });
 };
 
-// get the state of the connection: authenticated, disconnected
-function get_connection_state(){
-  return con.state;
-};
-
-//returns true or if the comment could be saved into the db successfully
-function add_comment(event_id, user_id, text){
-  var sqlstmt_beg = "INSERT INTO `event_finder`.`comments` (`event_id`, `user_id`, `comment`) VALUES ('";
-  con.query( sqlstmt_beg + event_id + "', '" + user_id + "', '"+ text + "');", function (err, result) {
+function add_comment(event_id, user_id, comment_text){
+  con.query(`INSERT INTO ${db_name}.comments (event_id, user_id, comment) VALUES ('${event_id}', '${user_id}', '${comment_text}');`, function (err, result) {
     if (err) {
       throw err;
     } else {
-      //console.log(result);
       return true;
     }
   });
-};
-//add_comment(1,1,"Test comment from node.js");
+}
 
-// get all saved_events from the logged in user
-function get_saved_events(user_id){
-  con.query( "SELECT * FROM event_finder.saved_events WHERE user_id = " + user_id + ";", function (err, result) {
+// get all cities with events
+function get_event_cities(){
+  con.query(`SELECT DISTINCT(city) FROM ${db_name}.events;`, function (err, result) {
     if (err) {
       throw err;
     } else {
       return result;
     }
   });
-};
+}
 
-//run onInit
-onInit();
+function get_events(start_date, end_date, event_type, search_phrase, city){
+  var sql = `SELECT * FROM ${db_name}.events WHERE `;
+  // if (start_date != null && end_date != null){
+  //   sql = sql + `((start_date BETWEEN "${start_date}" AND "${end_date}") OR (end_date BETWEEN "${start_date}" AND "${end_date}")) OR ((start_date BETWEEN "${start_date}" AND "${end_date}") AND (end_date BETWEEN "${start_date}" AND "${end_date}"))`;
+  // }
+  // if (event_type != null){
+  //   sql = sql + ``;
+  // }
+
+  if (city != null){
+    sql = sql + ` city = "${city}" `;
+  }
+
+  if (search_phrase != null){
+    var search_words = tokenize(search_phrase.toLowerCase());
+    for(var i = 0; i < search_words.length; i++){
+      sql = sql + ` LOWER(name) LIKE "%${search_words[i]}%" `;
+      if ( i + 1 < search_words.length){
+        sql = sql + " OR ";
+      }
+    }
+  }
+
+  sql = sql + ";";
+  console.log(sql);
+
+  con.query(sql, function (err, result) {
+    if (err) {
+      throw err;
+    } else {
+      console.log(result);
+      return result;
+    }
+  });
+}
+
+function get_saved_events(user_id){
+  return new Promise(function(resolve, reject) {
+    con.query(`SELECT * FROM ${db_name}.saved_events WHERE user_id = ${user_id};`, function (err, result) {
+      if (err) {
+        return reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+function add_event(name, description, city, zip, street, house_number, address_additional, instagram_hashtag, verification_status = 0, start_date, end_date, event_link, ticket_link){
+  return new Promise(function(resolve, reject) {
+    var sql = `INSERT INTO event_finder.events (name, description, city, zip, street, house_number, address_additional, instagram_hashtag, verification_status, start_date, end_date, event_link, ticket_link, event_type, times_reported) VALUES ('${name}', '${description}', '${city}', '${zip}', '${street}', '${house_number}', '${address_additional}', '${instagram_hashtag}', '${verification_status}', '${start_date}', '${end_date}', '${event_link}', '${ticket_link}', '${event_type}');`;
+    con.query(sql, function (err, result) {
+      if (err) {
+        return reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+get_saved_events(1).then(function(rows){
+  a = rows;
+});
+
+function tokenize(phrase){
+  phrase =  phrase.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]*/g, '');
+  phrase =  phrase.replace(/\s+/g, " ");
+  return phrase.split(" ");
+}
+// get_events("12.12.2018","31.12.2018",null, "TOLLES        Event!?&", null);
+// console.log(get_saved_events(1));
+
+
+// add_comment(2,1, "wow toll");
+
+
+
+
 //#################Unused Helpful Code Snippets#################################
 // // disconnect from database
 // con.end(function(err) {
