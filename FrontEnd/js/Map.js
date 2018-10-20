7de33d383adc9fb7ec7012c3114ab71d0f481234
cornelius.schaefer1@gmail.com <cornelius.schaefer1@gmail.com>
2018-10-20 03:11:00 +0200

    Marker setten möglich

--- FrontEnd/js/Map.js
+++ FrontEnd/js/Map.js
@@ -28,7 +28,6 @@ var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 var ui = H.ui.UI.createDefault(map, defaultLayers);
 
 
-
 /**
  * A full list of available request parameters can be found in the Geocoder API documentation.
  * see: http://developer.here.com/rest-apis/documentation/geocoder/topics/resource-geocode.html
@@ -56,15 +55,12 @@ function geocode(sQuery) {
             var oLatLgn = {lat: dLat, lng: dLng}
              
             map.setCenter(oLatLgn, true);
-            
             if(map.getZoom() < 14.5){
                 map.setZoom(14.5, true);
             }
             if(map.getZoom() > 17){
                 map.setZoom(16, true);
             }
-
-            // addLocationsToMap(locations);
         },
         /**
          * This function will be called if a communication error occurs during the JSON-P request
@@ -76,70 +72,57 @@ function geocode(sQuery) {
     );
 }
 
+//TODO - take care and fix the global variable + callback check
+var oCoords = undefined;
 function getCoordsByAddress(sAddress){
-    var oCoords;
-
+    var geocoder = platform.getGeocodingService(),
+    geocodingParameters = {
+        searchText: sAddress,
+        jsonattributes: 1
+    };
+    geocoder.geocode(
+    geocodingParameters,
+    onSuccess = function onSuccess(result) {
+        var dLat = result.response.view[0].result[0].location.displayPosition.latitude;
+        var dLng = result.response.view[0].result[0].location.displayPosition.longitude;
+        var oLatLgn = {lat: dLat, lng: dLng}
+        oCoords = oLatLgn;
+    },
+    onError = function(error) {
+        alert('Ooops!');
+    }
+    )
     return oCoords;
 }
 
 function setMarker(sAddress, sName){
+    var geocoder = platform.getGeocodingService(),
+    geocodingParameters = {
+        searchText: sAddress,
+        jsonattributes: 1
+    };
+    geocoder.geocode(
+    geocodingParameters,
+    onSuccess = function onSuccess(result) {
+        var dLat = result.response.view[0].result[0].location.displayPosition.latitude;
+        var dLng = result.response.view[0].result[0].location.displayPosition.longitude;
+        var oLatLgn = {lat: dLat, lng: dLng}
 
-    var oCoords = getCoordsByAddress(sAddress);
+        var marker = new H.map.Marker(oLatLgn);
+        marker.label = sName;
 
-    marker = new H.map.Marker(oCoords);
-    marker.label = sName;
+        map.addObject(marker);
 
-    marker.addEventListener('tap', function (evt) {
+        marker.addEventListener('tap', function (evt) {
         map.setCenter(evt.target.getPosition());
         openBubble(
             evt.target.getPosition(), evt.target.label);
     }, false);
-
-}
-
-/**
- * Creates a series of H.map.Markers for each location found, and adds it to the map.
- * @param {Object[]} locations An array of locations as received from the
- *                             H.service.GeocodingService
- */
-function addLocationsToMap(locations) {
-    var group = new H.map.Group(),
-        position,
-        i;
-
-    // Add a marker for each location found
-    for (i = 0; i < locations.length; i += 1) {
-        position = {
-            lat: locations[i].location.displayPosition.latitude,
-            lng: locations[i].location.displayPosition.longitude
-        };
-        marker = new H.map.Marker(position);
-
-                            /** alles Fake Daten
-                            var FakeEventMA1 = new H.map.Marker({ lat: 49.47689, lng: 8.46798 });
-                            map.addObject(FakeEventMA1);
-                            var FakeEventMA2 = new H.map.Marker({ lat: 49.5, lng: 8.465 });
-                            map.addObject(FakeEventMA2);
-                            var FakeEventMA3 = new H.map.Marker({ lat: 49.44, lng: 8.462 });
-                            map.addObject(FakeEventMA3);
-                            var FakeEventMA4 = new H.map.Marker({ lat: 49.41, lng: 8.4671 });
-                            map.addObject(FakeEventMA4);
-                            var FakeEventMA5 = new H.map.Marker({ lat: 49.49, lng: 8.4672 });
-                            map.addObject(FakeEventMA5);
-                            */
-
-        marker.label = locations[i].location.address.label;
-        group.addObject(marker);
+    },
+    onError = function(error) {
+        alert('Ooops!');
     }
-
-    group.addEventListener('tap', function (evt) {
-        map.setCenter(evt.target.getPosition());
-        openBubble(
-            evt.target.getPosition(), evt.target.label);
-    }, false);
-
-    // Add the locations group to the map
-    map.addObject(group);
+    )
 }
 
 var bubble; // Hold a reference to any infobubble opened
@@ -160,7 +143,6 @@ function openBubble(position, text) {
         bubble.open();
     }
 }
-
 
 // update map size at window size
 window.addEventListener('resize', function () {
