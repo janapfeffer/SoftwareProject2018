8257c4fc6f58d8cc03d514ef8c4a609783b1168d
Yuelingl <yuelingl@yahoo.de>
2018-10-25 17:41:24 +0200

    image uploads

--- Backend/RestAPI/models/event_model.js
+++ Backend/RestAPI/models/event_model.js
@@ -4,14 +4,17 @@ const oEventSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     event_name: { type: String, required: true },
     description: { type: String, required: true },
-    address:{
-    city: { type: String, required: true },
-    zip: { type: Number, required: true },
-    street: { type: String, required: true },
-    house_number: { type: Number, required: true },
-    address_additional: { type: String, required: false },
-    loc: { longitude: Number, latitude: Number },
+    
+    address: {
+        city: { type: String, required: true },
+        zip: { type: Number, required: true },
+        street: { type: String, required: true },
+        house_number: { type: Number, required: true },
+        address_additional: { type: String, required: false },
+        loc: { longitude: Number, latitude: Number },
     },
+    
+    event_picture: { type: String, required: false },
     instagram_hashtag: { type: String, required: false },
     verification_status: { type: Boolean, default: false },
     start_date: { type: Date, required: true },
@@ -20,6 +23,7 @@ const oEventSchema = mongoose.Schema({
     ticket_link: { type: String, required: false },
     event_types: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event_Type", required: true }],
     times_reported: { type: Number, default: 0 },
+    
     comments: [{
         username: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
         comment: { type: String, required: true }
