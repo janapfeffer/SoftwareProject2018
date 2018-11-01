683408772d96895aac6c5b27cf99dd4c465f35af
hannasmuffins <43777474+hannasmuffins@users.noreply.github.com>
2018-11-01 14:39:18 +0100

    login funktioniert halb
    lol

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -374,9 +374,39 @@ var oNewLoginVue = new Vue({
 
 
         formsubmit: function () {
+            
+            var suserlogin = "http://localhost:3000/user/login"
+            var ajaxRequest = new XMLHttpRequest();
+
+           
+            var onSuccess = function onSuccess() {
+           
+                console.log(this.status);
+                if (this.status == 200) {
+                    alert('Du bist erfolgreich eingeloggt');
+                } else {
+                    alert('Anmeldung nicht erfolgreich');
+
+                }
+
+            };
+            var onFailed = function onFailed() {
+                alert(' SO NE SCHEISSE');
+            };
+
+            
+
+            ajaxRequest.addEventListener("load", onSuccess);
+            ajaxRequest.addEventListener("error", onFailed);
+            ajaxRequest.responseType = "json";
+            ajaxRequest.open("POST", suserlogin, true);
+            ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            var suserdata = "email=" + this.draft.sUserName + "&password=" + this.draft.sPassword;
+            
+            ajaxRequest.send(suserdata);
+            console.log(suserdata);
 
             this.cardShown = !this.cardShown;
-
         },
 
 
