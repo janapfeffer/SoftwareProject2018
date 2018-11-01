a9adc8e1a030f10debee1ab0b09d9ae6e52c6602
hannasmuffins <43777474+hannasmuffins@users.noreply.github.com>
2018-11-01 14:00:40 +0100

    SIGN UP GEHT JUHU
    ENDLICH

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -311,6 +311,7 @@ var oRegisterVue = new Vue({
         cardShown: false,
         draft: {
             rUserName: "",
+            rEmail: "",
             rPassword: "",
             rPassword2: "",
             status: "draft",
@@ -325,11 +326,29 @@ var oRegisterVue = new Vue({
             }
         },
         formsubmit: function () {
+            var onSuccess = function onSuccess() {
+                alert('ich glaube es hat geklappt. HTTP CODE ABFANGEN WEIL EVTL HAT ES NED GEKLAPPT LOL');
+            };
+            var onFailed = function onFailed() {
+                alert(' SO NE SCHEISSE');
+            };
 
+            
             this.cardShown = !this.cardShown;
             oRegisterVue.cardShown = false;
             oNewLoginVue.cardShown = false;
 
+            var newuser = "http://localhost:3000/user/signup"
+            var ajaxRequest = new XMLHttpRequest();
+            
+            ajaxRequest.addEventListener("load", onSuccess);
+            ajaxRequest.addEventListener("error", onFailed);
+            ajaxRequest.responseType = "json";
+            ajaxRequest.open("POST", newuser, true);
+            ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            var snewuserdata = "name=" + this.draft.rUserName + "&email=" + this.draft.rEmail + "&password=" + this.draft.rPassword;
+            console.log(snewuserdata);
+            ajaxRequest.send(snewuserdata);
         },
 
     }
