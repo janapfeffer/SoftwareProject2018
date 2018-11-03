c5a28ffece1d372233d82829a63904a804f40752
cornelius.schaefer1@gmail.com <cornelius.schaefer1@gmail.com>
2018-11-03 02:18:24 +0100

    ddd

--- FrontEnd/js/Map.js
+++ FrontEnd/js/Map.js
@@ -315,7 +315,7 @@ var bubble; // Hold a reference to any infobubble opened
 function openBubble(position, oData) {
     var myHTMLcontent = 
     "<div class=\infoBubble\>" +
-        "<div class=\ibPicture\ style='background-image: url(" + oData.oImage + ");></div>" +
+        "<div class=\ibPicture\ style='background-image: url(" + oData.oImage + ")';></div>" +
         "<div class=\ibText\>" +
             "<span>" + oData.sName + "</span>" +
         "</div>" +   
