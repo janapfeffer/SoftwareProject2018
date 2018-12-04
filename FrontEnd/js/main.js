bc718693ab6516e422de05dcf1e489559f1475b3
Yuelingl <yuelingl@yahoo.de>
2018-12-04 20:44:41 +0100

    Alignments

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -798,7 +798,7 @@ var oSearchPlaceVue = new Vue({
     methods: {
         //Sucht nach einem Ort
         newText: text=>{
-            return "+" +text + " weitere ausgewählt.";
+            return "+" +text + " Tags";
         },
         searchPlace: function searchPlace() {
             if (document.body.classList.contains('landingpage')) {
